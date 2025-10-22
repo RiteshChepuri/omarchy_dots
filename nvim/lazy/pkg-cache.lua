e326e213ab6d632647d68bc662abc5cb18d8abd7
RiteshChepuri <riteshchepuri0@gmail.com>
2025-10-22 12:22:53 +0530

    Add files via upload

---
+++ nvim/lazy/pkg-cache.lua
@@ -0,0 +1,10 @@
+return {version=12,pkgs={{dir="/home/ritesh/.local/share/nvim/lazy/noice.nvim",file="lazy.lua",spec=function()
+return {
+  -- nui.nvim can be lazy loaded
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "folke/noice.nvim",
+  },
+}
+
+end,source="lazy",name="noice.nvim",},{dir="/home/ritesh/.local/share/nvim/lazy/plenary.nvim",file="community",spec={"nvim-lua/plenary.nvim",lazy=true,},source="lazy",name="plenary.nvim",},},}
